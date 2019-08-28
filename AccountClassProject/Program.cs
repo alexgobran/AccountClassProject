7bfd9824652de702a776dafbcefae13159b943f0
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-08-28 09:28:01 -0400

    intial commit

--- AccountClassProject/Program.cs
+++ AccountClassProject/Program.cs
@@ -3,7 +3,7 @@
 namespace AccountClassProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Young World!");
         }
     }
 }
